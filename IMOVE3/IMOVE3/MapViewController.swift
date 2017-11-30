e11a1d1929dc1de2da361c334021c07e44bdb08e
Hendriks,Loe L.F.G <loe.hendriks@student.fontys.nl>
2017-11-30 09:55:58 +0100

    guide tableview added

--- IMOVE3/IMOVE3/MapViewController.swift
+++ IMOVE3/IMOVE3/MapViewController.swift
@@ -14,17 +14,54 @@ class MapViewController: UIViewController, CLLocationManagerDelegate {
     @IBOutlet weak var Map: MKMapView!
     @IBOutlet weak var ButtonRecenter: UIButton!
     var locationManager = CLLocationManager()
-    
+    var myLocation = CLLocationCoordinate2D()
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        ButtonRecenter.layer.cornerRadius = 4
-        ButtonRecenter.isHidden = true
-        locationManager.delegate = self
-       locationManager.requestWhenInUseAuthorization()
-        locationManager.desiredAccuracy = kCLLocationAccuracyBest
-        locationManager.startUpdatingLocation()
+        ButtonFeatures()
+        
+        LocationManagerStartUp()
+        //SetChallenges()
+        
+        
+        
+        let challengeLocation = CLLocationCoordinate2D(latitude: 51.44950853626378, longitude: 5.479984190315008)
+        let runChallenge1 = MKPointAnnotation()
+        runChallenge1.coordinate = challengeLocation
+        runChallenge1.title = "Running Challenge"
+        runChallenge1.subtitle = "Run 4km"
+        Map.addAnnotation(runChallenge1)
+        let run1Placemark = MKPlacemark(coordinate: challengeLocation, addressDictionary: nil)
+        let run1MapItem = MKMapItem(placemark: run1Placemark)
+        
+        let locationPlacemark = MKPlacemark(coordinate: myLocation, addressDictionary: nil)
+        let locationMapItem = MKMapItem(placemark: locationPlacemark)
+        
+        
+        let directionRequest = MKDirectionsRequest()
+        directionRequest.source = locationMapItem
+        directionRequest.destination = run1MapItem
+        directionRequest.transportType = .walking
+        
+        let directions = MKDirections(request: directionRequest)
+        directions.calculate {
+            (response, error) -> Void in
+            
+            guard let response = response else {
+                if let error = error {
+                    print("Error: \(error)")
+                }
+                
+                return
+            }
+            
+            let route = response.routes[0]
+            self.Map.add((route.polyline), level: MKOverlayLevel.aboveRoads)
+            
+            let rect = route.polyline.boundingMapRect
+            self.Map.setRegion(MKCoordinateRegionForMapRect(rect), animated: true)
+        }
     }
 
     override func didReceiveMemoryWarning() {
@@ -62,10 +99,35 @@ class MapViewController: UIViewController, CLLocationManagerDelegate {
         let location = locations[0]
         
         let span = MKCoordinateSpanMake(0.01, 0.01)
-        let myLocation:CLLocationCoordinate2D = CLLocationCoordinate2DMake(location.coordinate.latitude, location.coordinate.longitude)
+        myLocation = CLLocationCoordinate2DMake(location.coordinate.latitude, location.coordinate.longitude)
         let region:MKCoordinateRegion = MKCoordinateRegionMake(myLocation, span)
         Map.setRegion(region, animated: true)
         self.Map.showsUserLocation = true
     }
+    
+    func ButtonFeatures()
+    {
+        ButtonRecenter.layer.cornerRadius = 4
+        ButtonRecenter.isHidden = true
+    }
+    
+    func LocationManagerStartUp()
+    {
+        locationManager.delegate = self
+        locationManager.requestWhenInUseAuthorization()
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        locationManager.startUpdatingLocation()
+    }
+    
+    func SetChallenges()
+    {
+        let challengeLocation = CLLocationCoordinate2D(latitude: 51.44950853626378, longitude: 5.479984190315008)
+        let runChallenge1 = MKPointAnnotation()
+        runChallenge1.coordinate = challengeLocation
+        runChallenge1.title = "Running Challenge"
+        runChallenge1.subtitle = "Run 4km"
+        Map.addAnnotation(runChallenge1)
+    }
+    
 
 }
