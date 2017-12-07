fd35db557d42ce805ab66b9c29f802832921d621
Teurlings,Bram B.F.M <b.teurlings@student.fontys.nl>
2017-12-07 12:51:07 +0100

    Fixed navigation for challenges

--- IMOVE3/IMOVE3/CustomAnnotationView.swift
+++ IMOVE3/IMOVE3/CustomAnnotationView.swift
@@ -14,8 +14,14 @@ class CustomAnnotationView: UIView {
     @IBOutlet weak var challengeDescription: UILabel!
     @IBOutlet weak var navigateButton: UIButton!
     
+    let storyBoard = UIStoryboard(name: "Main", bundle: nil)
+    
     @IBAction func ClickStart(_ sender: Any) {
-      
+        
+        let nextViewController = storyBoard.instantiateViewController(withIdentifier: "Challenge") as! UITabBarController
+       
+    UIApplication.topViewController()?.present(nextViewController, animated: true, completion: nil)
+
     }
     /*
     // Only override draw() if you perform custom drawing.
@@ -25,4 +31,21 @@ class CustomAnnotationView: UIView {
     }
     */
 
+}
+
+extension UIApplication {
+    class func topViewController(base: UIViewController? = UIApplication.shared.keyWindow?.rootViewController) -> UIViewController? {
+        if let nav = base as? UINavigationController {
+            return topViewController(base: nav.visibleViewController)
+        }
+        if let tab = base as? UITabBarController {
+            if let selected = tab.selectedViewController {
+                return topViewController(base: selected)
+            }
+        }
+        if let presented = base?.presentedViewController {
+            return topViewController(base: presented)
+        }
+        return base
+    }
 }
